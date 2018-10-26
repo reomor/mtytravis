01e902eb44fc78d6aa5982b4209150ffaac18497
Reomor <reomor@gmail.com>
2018-10-26 10:36:20 -0400

    HW06 main task add two input vatiables

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,16 +5,16 @@ provider "google" {
 }
 
 resource "google_compute_instance" "app" {
-  name	       = "reddit-app"
+  name         = "reddit-app"
   machine_type = "g1-small"
-  zone         = "europe-west1-b"
+  zone         = "${var.zone}"
 
   tags = ["reddit-app"]
-  
+
   metadata {
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
-  
+
   boot_disk {
     initialize_params {
       image = "${var.disk_image}"
@@ -22,14 +22,14 @@ resource "google_compute_instance" "app" {
   }
 
   connection {
-    type = "ssh"
-    user = "appuser"
-    agent = false
-    private_key = "${file("~/.ssh/appuser")}"
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
   }
 
   provisioner "file" {
-    source = "files/puma.service"
+    source      = "files/puma.service"
     destination = "/tmp/puma.service"
   }
 
@@ -40,6 +40,7 @@ resource "google_compute_instance" "app" {
   network_interface {
     # network to connect
     network = "default"
+
     # use ephemeral IP
     access_config {}
   }
@@ -47,15 +48,19 @@ resource "google_compute_instance" "app" {
 
 resource "google_compute_firewall" "firewall_puma" {
   name = "allow-puma-default"
+
   # network name, where rule is active
   network = "default"
+
   # allowed port
   allow {
     protocol = "tcp"
-    ports = ["9292"]
+    ports    = ["9292"]
   }
+
   # allowed addresses
   source_ranges = ["0.0.0.0/0"]
+
   # instance tags, where rule is active
   target_tags = ["reddit-app"]
 }
