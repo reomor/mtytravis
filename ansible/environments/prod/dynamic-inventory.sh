b6148b40ed8c373e8db211d91d2eac6aa637bf0a
Reomor <reomor@gmail.com>
2018-12-02 12:04:15 -0500

    HW10star dyn inventory for prod

--- ansible/environments/prod/dynamic-inventory.sh
+++ ansible/environments/prod/dynamic-inventory.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-set -e
-./dynamic-inventory/gce.py --list | sed 's/reddit-db/db/g; s/reddit-app/app/g'
+./dynamic-inventory/gce.py --list | sed 's/tag_reddit-db/db/g; s/tag_reddit-app/app/g; s/reddit-db/db/g; s/reddit-app/app/g'
